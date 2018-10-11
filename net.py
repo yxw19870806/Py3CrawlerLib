aaabe1349483d67f7bfd24e0b15cb801664ef139
hikaru <hikaru870806@hotmail.com>
2018-10-11 11:50:04 +0800

    1.增加可选参数，控制是否使用gzip压缩，并且对相应报错进行处理 2.增加url_encode方法，对非支持字符进行Percent_encoding

--- net.py
+++ net.py
@@ -13,6 +13,7 @@ import re
 import ssl
 import time
 import threading
+import urllib.parse
 import urllib3
 
 try:
@@ -135,8 +136,14 @@ def get_file_type(file_url, default_file_type=""):
         return file_name_and_type[-1]
 
 
-def http_request(url, method="GET", fields=None, binary_data=None, header_list=None, cookies_list=None, encode_multipart=False, is_auto_proxy=True, is_auto_redirect=True,
-                 is_auto_retry=True, connection_timeout=NET_CONFIG["HTTP_CONNECTION_TIMEOUT"], read_timeout=NET_CONFIG["HTTP_READ_TIMEOUT"], is_random_ip=True, json_decode=False):
+def url_encode(url):
+    # encode url(Percent-encoding), e.g. 'http://www.example.com/测 试/' -> 'http://www.example.com/%E6%B5%8B%20%E8%AF%95/'
+    return urllib.parse.quote(url, safe=";/?:@&=+$,%")
+
+
+def http_request(url, method="GET", fields=None, binary_data=None, header_list=None, cookies_list=None, encode_multipart=False, json_decode=False,
+                 is_auto_proxy=True, is_auto_redirect=True, is_gzip=True, is_url_encode=True, is_auto_retry=True, is_random_ip=True,
+                 connection_timeout=NET_CONFIG["HTTP_CONNECTION_TIMEOUT"], read_timeout=NET_CONFIG["HTTP_READ_TIMEOUT"]):
     """Http request via urllib3
 
     :param url:
@@ -193,6 +200,8 @@ def http_request(url, method="GET", fields=None, binary_data=None, header_list=N
     connection_pool = HTTP_CONNECTION_POOL
     if PROXY_HTTP_CONNECTION_POOL is not None and is_auto_proxy:
         connection_pool = PROXY_HTTP_CONNECTION_POOL
+    if is_url_encode:
+        url = url_encode(url)
 
     if header_list is None:
         header_list = {}
@@ -212,7 +221,8 @@ def http_request(url, method="GET", fields=None, binary_data=None, header_list=N
         header_list["Cookie"] = build_header_cookie_string(cookies_list)
 
     # 设置压缩格式
-    header_list["Accept-Encoding"] = "gzip"
+    if is_gzip:
+        header_list["Accept-Encoding"] = "gzip"
 
     # 超时设置
     if connection_timeout == 0 and read_timeout == 0:
@@ -235,13 +245,15 @@ def http_request(url, method="GET", fields=None, binary_data=None, header_list=N
                 response = connection_pool.request(method, url, headers=header_list, redirect=is_auto_redirect, timeout=timeout, fields=fields)
             else:
                 if binary_data is None:
-                    response = connection_pool.request(method, url, fields=fields, encode_multipart=encode_multipart, headers=header_list, redirect=is_auto_redirect, timeout=timeout)
+                    response = connection_pool.request(method, url, fields=fields, encode_multipart=encode_multipart, headers=header_list,
+                                                       redirect=is_auto_redirect, timeout=timeout)
                 else:
-                    response = connection_pool.request(method, url, body=binary_data, encode_multipart=encode_multipart, headers=header_list, redirect=is_auto_redirect, timeout=timeout)
+                    response = connection_pool.request(method, url, body=binary_data, encode_multipart=encode_multipart, headers=header_list,
+                                                       redirect=is_auto_redirect, timeout=timeout)
             if response.status == HTTP_RETURN_CODE_SUCCEED and json_decode:
                 try:
                     response.json_data = json.loads(response.data.decode())
-                except ValueError as ve:
+                except ValueError:
                     is_error = True
                     content_type = response.getheader("Content-Type")
                     if content_type is not None:
@@ -282,6 +294,12 @@ def http_request(url, method="GET", fields=None, binary_data=None, header_list=N
             elif isinstance(e, urllib3.exceptions.MaxRetryError):
                 if message.find("Caused by ResponseError('too many redirects'") >= 0:
                     return ErrorResponse(HTTP_RETURN_CODE_TOO_MANY_REDIRECTS)
+            elif isinstance(e, urllib3.exceptions.DecodeError):
+                if message.find("'Received response with content-encoding: gzip, but failed to decode it.'") >= 0:
+                    return http_request(url, method=method, fields=fields, binary_data=binary_data, header_list=header_list, cookies_list=cookies_list,
+                                        encode_multipart=encode_multipart, json_decode=json_decode, is_auto_proxy=is_auto_proxy, is_auto_redirect=is_auto_redirect,
+                                        is_gzip=False, is_url_encode=False, is_auto_retry=is_auto_retry, is_random_ip=is_random_ip,
+                                        connection_timeout=connection_timeout, read_timeout=read_timeout)
             # output.print_msg(message)
             # output.print_msg(traceback.format_exc())
             output.print_msg(url + " 访问超时，重试中")
